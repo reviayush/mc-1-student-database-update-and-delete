9c731c84421b27d5ef3b7864faddb73f4301489c
ayush8802396070@gmail.com <ayush8802396070@gmail.com>
2022-11-19 16:36:47 +0530

    mc-1 database assignment

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -8,29 +8,64 @@ import java.sql.*;
  */
 public class DeleteUpdateStudentData
 {
+    static Connection connection= null;
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        //Connection connection= null;
 
-    public Connection getConnection(){
-        Connection connection=null;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/school";
+        String user = "root";
+        String password = "password@123";
+        connection = DriverManager.getConnection(url, user, password);
+
 
         return connection;
 
     }
 
-    public void deleteStudentData(){
+    public Student deleteStudentData(int studid) throws SQLException, ClassNotFoundException {
       // call getConnection() method
-       
+        Student stud= null;
+        Connection connection  = DeleteUpdateStudentData.getConnection();
+        String sql = "select * from student where studid = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1,studid );
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while(resultSet.next()){
+            int studentid = resultSet.getInt(1);
+            stud = new Student(studid);
+
+        }
+
+        return stud;
+
         //execute delete query
     }
 
-    public void updateStudentData(){
+    public int updateStudentData(int studid) throws SQLException, ClassNotFoundException {
         // call getConnection() method
+        int studen =0;
+        Connection connection = DeleteUpdateStudentData.getConnection();
+        String sql = "update student set marks=? where studid=?";//preparing a query with host variable or argument or parameter
+        //parameters having index or position starting from 1
+        //declare PreparedStatement
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
+        //set values for parameters
+        //setXXX method of prepared statement
+        preparedStatement.setInt(1, 300);
+        preparedStatement.setInt(2,studid);
+
+        //execute query
+        studen = preparedStatement.executeUpdate();
+
+        return studen;
         //execute update query
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         DeleteUpdateStudentData salesDataDemo = new DeleteUpdateStudentData();
-        salesDataDemo.deleteStudentData();
-        salesDataDemo.updateStudentData();
+        salesDataDemo.deleteStudentData(10);
+        salesDataDemo.updateStudentData(1);
     }
 }
---
+++ src/main/java/com/jap/Student.java
@@ -0,0 +1,57 @@
+package com.jap;
+
+public class Student {
+
+    int id;
+    String name;
+    int rollno;
+    int marks;
+
+    public Student(int studid) {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rollno=" + rollno +
+                ", marks=" + marks +
+                '}';
+    }
+
+
+}
